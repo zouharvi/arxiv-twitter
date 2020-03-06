9bd69a4c1fd5b3554b47df30de2243358d7b52e6
Vilem Zouhar <vilem.zouhar@gmail.com>
2020-03-06 11:59:06 +0100

    add hashtaging

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import logging
 import requests
 import re
 import twitter
+import nltk
 
 log = logging.getLogger()
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
@@ -32,6 +33,9 @@ CONSUMER_SECRET = 'a'
 ACCESS_TOKEN_KEY = 'a'
 ACCESS_TOKEN_SECRET = 'a'
 
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+
 def parse_articles(xml):
     """
     For the given string of arXiv.org RSS XML, return list of Articles
@@ -46,6 +50,28 @@ def parse_articles(xml):
         ] for item in items
     ]
 
+def add_hashtags(abstract):
+    MIN_HASHTAGIFY = 10
+
+    hashtags = set()
+    tokens = nltk.word_tokenize(abstract)
+    tags = nltk.pos_tag(tokens)
+    for word, tag in tags:
+        if word[0] == '0':
+            continue
+        if '-' in word:
+            continue
+        if (len(word) >= 3 and all([x.isupper() for x in word])) or \
+           (len(word) >= MIN_HASHTAGIFY and (tag in ['NN', 'NNP', 'NNS', 'JJ'])):
+            hashtags.add(word)
+
+    for hashtag in hashtags:
+        abSp = abstract.split(hashtag)
+        # we add hashtags to the first word only
+        abstract = abSp[0] + '#' + hashtag + hashtag.join(abSp[1:])
+    
+    return abstract
+
 def generate_tweet(article):
     """
     Construct a tweet for the given article
@@ -55,31 +81,37 @@ def generate_tweet(article):
     # Take the proper title
     title = article[0].split('. (arXiv')[0]
     # Twitter links are 23 characters
-    linkPlaceholder = '#'*23
+    linkPlaceholder = '0'*23
     link = article[1]
     # Abstract
     abstract = article[2]
 
     out = f'{title} {linkPlaceholder} {abstract}'    
+    out = add_hashtags(out)
+
     out = out[:MAX_CHAR]
     out = out.replace(linkPlaceholder, link)
 
     out = re.sub(r'<.*?>', ' ', out)
     out = re.sub(r'\n+', ' ', out)
     out = re.sub(r'\s+', ' ', out)
+    out = re.sub(r'\#+', '#', out)
+    out = re.sub(r'(\w)\#', r'\1', out)
+    
+    out = out[:MAX_CHAR]
 
     # Go back and remove everything after the last end of word/phrase/sentence
     out = re.sub(r'(\.|\,|\?|\s)[^\.\,\?\s]*$', r'-', out)
-    while len(out) > MAX_CHAR:
-        out = re.sub(r'(\.|\,|\?|\s)[^\.\,\?\s]*$', r'-', out)
+    # while len(out) > MAX_CHAR:
+    #     out = re.sub(r'(\.|\,|\?|\s)[^\.\,\?\s]*$', r'-', out)
     
     return out
-
 
 def send_tweet(api, tweet):
     """
     Actually POST tweet to twitter API
     """
+    return
     try:
         api.PostUpdate(tweet)
         log.info(f'Sent "{tweet}"')
@@ -103,5 +135,5 @@ if __name__ == '__main__':
         )
 
         for article in parse_articles(res.text):
-            print(generate_tweet(article))
+            print(generate_tweet(article), '\n')
             send_tweet(api, generate_tweet(article))
